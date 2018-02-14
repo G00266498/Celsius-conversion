5a427ad0d3b55cbd51e03d89faa0fd14272acd9a
Totalocura <35809338+Totalocura@users.noreply.github.com>
2018-02-14 15:00:59 +0000

    celsius_ conversion

---
+++ js/convertor.js
@@ -0,0 +1,13 @@
+function cToF(){
+	
+	var cTemp = parseInt(document.getElementById("value1").value);
+	
+	var cToFahr = cTemp * 9 / 5 + 32;
+	
+	var message = cTemp+ '\xB0C is ' + cToFahr + '\xB0C .';
+	console.log(message);
+	document.getElementById("resultsentence").innerHTML = message;
+}
+
+
+	
